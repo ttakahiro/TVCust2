c5c53fdd275b57ae7238788507c36ee6595e211a
takahiro <takahiro.takahashi@accelsofteng.com>
2019-06-04 23:13:40 +0900

    プロジェクト ファイルを追加します。

---
+++ TVCust/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TVCust
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public List<Person> Children { get; set; }
+    }
+
+    public class MyDataContext2 : INotifyPropertyChanged
+    {
+        public MyDataContext2()
+        {
+            Persons = new List<Person>()
+            {
+                new Person
+                {
+                    Name = "テスト１",
+                    Children = new List<Person>()
+                    {
+                        new Person{ Name = "テスト２"},
+                        new Person{ Name = "テスト３"},
+                    }
+                },
+                new Person
+                {
+                    Name = "hoge",
+                    Children = new List<Person>()
+                    {
+                        new Person{ Name = "foo" },
+                        new Person{
+                            Name = "bar",
+                            Children = new List<Person>
+                            {
+                                new Person{ Name = "last" }
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string name = null)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(storage, value))
+                return false;
+            storage = value;
+            this.RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        private IEnumerable<Person> _persons;
+        public IEnumerable<Person> Persons
+        {
+            get { return _persons; }
+            private set
+            {
+                SetProperty(ref _persons, value);
+            }
+        }
+    }
+
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TVCust/TreeViewControl2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Collections;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace TVCust
+{
+    public class TreeViewControl2 : Control
+    {
+        static TreeViewControl2()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TreeViewControl2), new FrameworkPropertyMetadata(typeof(TreeViewControl2)));
+        }
+
+
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ItemsSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(TreeViewControl2), new PropertyMetadata(null));
+
+
+
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ItemTemplate.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemTemplateProperty =
+            DependencyProperty.Register("ItemTemplate", typeof(DataTemplate), typeof(TreeViewControl2), new PropertyMetadata(null));
+
+
+        public TreeViewControl2()
+        {
+
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            var ctrl = GetTemplateChild("treebase") as TreeView;
+            if (ctrl != null)
+            {
+                _treeView = ctrl;
+            }
+        }
+
+        private TreeView _treeView;
+    }
+
+    public class LeftMarginMultiplierController : IValueConverter
+    {
+        public double Length { get; set; } = 19;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var item = value as TreeViewItem;
+            if (item == null)
+                return null;
+
+            var leftOffset = Length * item.GetDepth();
+            var ret = new Thickness(leftOffset, 0, 0, 0);
+
+            return ret;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public static class TreeViewItemExtensions
+    {
+        public static int GetDepth(this TreeViewItem item)
+        {
+            TreeViewItem parent;
+            while ((parent = GetParent(item)) != null)
+            {
+                return GetDepth(parent) + 1;
+            }
+            return 0;
+        }
+        private static TreeViewItem GetParent(TreeViewItem item)
+        {
+            var parent = VisualTreeHelper.GetParent(item);
+            while (!(parent is TreeViewItem || parent is TreeView))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+            return parent as TreeViewItem;
+        }
+    }
+}
